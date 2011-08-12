bd5dccde8787893e433e08ffa60a6c5df9086944
Acerbic <kuzzdra@gmail.com>
2011-08-13 00:44:28 +0400

    more tests on AbstractPage

--- test/dloader/AbstractPageTest.java
+++ test/dloader/AbstractPageTest.java
@@ -61,10 +61,10 @@ public class AbstractPageTest {
 		assertEquals("http://www.google.com", page.url.toString());
 	}
 
-	@Test
-	public void testGetFSSafeName() {
-		fail("Not yet implemented"); // TODO
-	}
+//	@Test
+//	public void testGetFSSafeName() {
+//		fail("Not yet implemented"); 
+//	}
 
 	@Test
 	public void testDownloadPageDummyFromLocal() throws ProblemsReadingDocumentException {
@@ -77,21 +77,42 @@ public class AbstractPageTest {
 	}
 
 	@Test
-	public void testLoadFromCache() {
-		fail("Not yet implemented"); // TODO
+	public void testLoadFromCacheFailsOnNullArg() {
+		AbstractPage p = new AbstractPageDummy("file:///D:/Gleb/JavaWorkspace_Eclipse/Dloader/test/Homestuck.htm");
+		assertEquals(false, p.loadFromCache(null));
 	}
+	
+	@Test
+	public void testLoadFromCacheFailsIfCantFindElement() {
+		AbstractPage p = new AbstractPageDummy("http://homestuck.bandcamp.com");
+		XMLCache cache = new XMLCache("test/pages_scan_cache.xml");
+		assertEquals(false, p.loadFromCache(cache.doc));
+		
+		AbstractPage p_discography = PageProcessor.detectPage("http://unknownband.bandcamp.com");
+		assertEquals(false, p_discography.loadFromCache(cache.doc));
+	}	
 
 	@Test
-	public void testQueryXPathListStringDocument() {
-		fail("Not yet implemented"); // TODO
+	public void testLoadFromCacheSuccessOnLeafPage() {
+		AbstractPage pNoChildren = PageProcessor.detectPage("http://homestuck.bandcamp.com/track/black-rose-green-sun");
+		XMLCache cache = new XMLCache("test/pages_scan_cache.xml");
+		assertEquals(true, pNoChildren.loadFromCache(cache.doc));
+		assertEquals("Black Rose / Green Sun", pNoChildren.title);
+		assertNull(pNoChildren.parent);
+		assertNull(pNoChildren.childPages);
 	}
-
+	
 	@Test
-	public void testReadCacheChild() {
-		fail("Not yet implemented"); // TODO
+	public void testLoadFromCacheSuccessOnCustomPage() {
+		AbstractPage p = PageProcessor.detectPage("http://noctura.bandcamp.com/album/demos");
+		XMLCache cache = new XMLCache("test/pages_scan_cache.xml");
+		assertEquals(true, p.loadFromCache(cache.doc));
+		assertEquals("Demos", p.title);
+		assertNotNull(p.childPages);
+		assertEquals(7, p.childPages.length);
+		assertEquals("http://noctura.bandcamp.com/track/dont-save-me-acoustic-for-x103", p.childPages[5].url.toString());
 	}
-
-
+	
 	@Test
 	public void testSaveToCache() {
 		fail("Not yet implemented"); // TODO
