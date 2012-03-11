ef04904b1bd64e2e45584d1919af26f9bab497d4
Acerbic <acerbic@mail.ru>
2012-03-11 07:07:39 +0400

    FindUnwantedFiles not finished

---
+++ src/dloader/pagejob/PageToFilesMapping.java
@@ -0,0 +1,19 @@
+package dloader.pagejob;
+
+import java.nio.file.Path;
+import java.util.Set;
+import java.util.HashSet;
+
+import dloader.page.AbstractPage;
+
+public class PageToFilesMapping {
+	
+	public final AbstractPage page;
+	public final Set<Path> pathSet;
+	
+	PageToFilesMapping(AbstractPage p) {
+		page = p;
+		pathSet = new HashSet<Path>();
+	}
+
+}
